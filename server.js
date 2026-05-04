9aeb6f482981b153fb4972a6b455dcfd726ab91e
Ananda Riangsari <ariangsari07@gmail.com>
2026-05-04 20:12:29 +0700

    update port for deploy

--- server.js
+++ server.js
@@ -54,8 +54,10 @@ app.get("/data", function(req,res){
 
 });
 
-app.listen(3000, function(){
-    console.log("Server berjalan di http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+    console.log("Server berjalan di port " + PORT);
 });
 
 // HAPUS DATA
